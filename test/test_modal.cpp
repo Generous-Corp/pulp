0ccc72812b21964f371d78d8f6ca6ff100e32921
Pulp Test <pulp-test@example.com>
2026-05-15 03:09:38 -0700

    test(view): cover control edge paths
    Version-Bump: skip

--- test/test_modal.cpp
+++ test/test_modal.cpp
@@ -2,6 +2,8 @@
 #include <pulp/view/modal.hpp>
 #include <pulp/canvas/canvas.hpp>
 
+#include <memory>
+
 using namespace pulp::view;
 using namespace pulp::canvas;
 
@@ -51,4 +53,49 @@ TEST_CASE("ModalOverlay non-Escape key not handled", "[view][modal]") {
     enter.is_down = true;
     REQUIRE_FALSE(modal.on_key_event(enter));
     REQUIRE_FALSE(dismissed);
+}
+
+TEST_CASE("ModalOverlay backdrop click dismisses only outside children",
+          "[view][modal][coverage][issue-653]") {
+    ModalOverlay modal;
+    modal.set_bounds({0, 0, 200, 100});
+    auto child = std::make_unique<View>();
+    child->set_bounds({25, 20, 50, 40});
+    modal.add_child(std::move(child));
+
+    int dismiss_count = 0;
+    modal.on_dismiss = [&] { ++dismiss_count; };
+
+    MouseEvent child_click;
+    child_click.position = {30, 25};
+    child_click.is_down = true;
+    modal.on_mouse_event(child_click);
+    REQUIRE(dismiss_count == 0);
+
+    MouseEvent backdrop_click;
+    backdrop_click.position = {150, 80};
+    backdrop_click.is_down = true;
+    modal.on_mouse_event(backdrop_click);
+    REQUIRE(dismiss_count == 1);
+
+    modal.dismiss_on_backdrop_click = false;
+    modal.on_mouse_event(backdrop_click);
+    REQUIRE(dismiss_count == 1);
+}
+
+TEST_CASE("ModalOverlay ignores mouse-up and missing dismiss callback",
+          "[view][modal][coverage][issue-653]") {
+    ModalOverlay modal;
+    modal.set_bounds({0, 0, 80, 40});
+
+    MouseEvent up;
+    up.position = {10, 10};
+    up.is_down = false;
+    modal.on_mouse_event(up);
+
+    MouseEvent down;
+    down.position = {10, 10};
+    down.is_down = true;
+    modal.on_mouse_event(down);
+    SUCCEED("modal mouse paths tolerate missing callbacks");
 }
