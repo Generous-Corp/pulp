e424fdf2359e24bf40bb762fcc83340f09fc1f9e
daniel raffel <daniel.raffel@gmail.com>
2026-06-13 23:32:33 -0700

    refactor(local-ci): trim CLI dispatch composer tests

--- tools/local-ci/test_cli_dispatch_bindings.py
+++ tools/local-ci/test_cli_dispatch_bindings.py
@@ -2,9 +2,9 @@
 """Tests for CLI dispatch compatibility facade bindings."""
 
 from module_test_utils import load_module_from_path
-import types
 import unittest
 from pathlib import Path
+from unittest import mock
 
 
 MODULE_PATH = Path(__file__).with_name("cli_dispatch_bindings.py")
@@ -29,131 +29,25 @@ class CliDispatchBindingsTests(unittest.TestCase):
         for name in self.mod.CLI_DISPATCH_EXPORTS:
             self.assertTrue(callable(getattr(self.mod, name)))
 
-    def test_install_cli_dispatch_helpers_wires_named_exports(self):
-        captured = {}
-        args = object()
-
-        def dispatch_desktop_command(value, *, commands):
-            captured["desktop_args"] = value
-            captured["desktop_commands"] = commands
-            return 22
-
-        def dispatch_main_command(value, **kwargs):
-            captured["main_args"] = value
-            captured["main_kwargs"] = kwargs
-            return 33
-
-        bindings = {
-            "_cli_dispatch": types.SimpleNamespace(
-                dispatch_desktop_command=dispatch_desktop_command,
-                dispatch_main_command=dispatch_main_command,
-            ),
-        }
-        for name in [
-            "cmd_desktop_install",
-            "cmd_desktop_doctor",
-            "cmd_desktop_status",
-            "cmd_desktop_config",
-            "cmd_desktop_recent",
-            "cmd_desktop_proof",
-            "cmd_desktop_publish",
-            "cmd_desktop_cleanup",
-            "cmd_desktop_smoke",
-            "cmd_desktop_click",
-            "cmd_desktop_inspect",
-            "cmd_enqueue",
-            "cmd_drain",
-            "cmd_run",
-            "cmd_ship",
-            "cmd_check",
-            "cmd_list",
-            "cmd_bump",
-            "cmd_cancel",
-            "cmd_logs",
-            "cmd_cleanup",
-            "cmd_evidence",
-            "cmd_status",
-            "cmd_cloud_workflows",
-            "cmd_cloud_defaults",
-            "cmd_cloud_history",
-            "cmd_cloud_compare",
-            "cmd_cloud_recommend",
-            "cmd_cloud_run",
-            "cmd_cloud_status",
-            "cmd_cloud_namespace_doctor",
-            "cmd_cloud_namespace_setup",
-        ]:
-            bindings[name] = object()
-
-        self.mod.install_cli_dispatch_helpers(bindings, ("cmd_desktop", "dispatch_main_command"))
-
-        self.assertEqual(bindings["cmd_desktop"](args), 22)
-        self.assertEqual(bindings["dispatch_main_command"](args, lambda: None), 33)
-        self.assertIs(captured["desktop_args"], args)
-        self.assertIs(captured["main_args"], args)
-
     def test_install_cli_dispatch_helpers_routes_each_group(self):
-        captured = {}
-        args = object()
+        bindings = {}
 
-        def dispatch_desktop_command(value, *, commands):
-            captured["desktop_args"] = value
-            captured["desktop_commands"] = commands
-            return 44
+        with (
+            mock.patch.object(self.mod, "install_cli_desktop_dispatch_helpers") as install_desktop,
+            mock.patch.object(self.mod, "install_cli_main_dispatch_helpers") as install_main,
+        ):
+            self.mod.install_cli_dispatch_helpers(bindings, ("cmd_desktop", "dispatch_main_command"))
 
-        def dispatch_main_command(value, **kwargs):
-            captured["main_args"] = value
-            captured["main_kwargs"] = kwargs
-            return 55
+        install_desktop.assert_called_once_with(bindings, ("cmd_desktop",))
+        install_main.assert_called_once_with(bindings, ("dispatch_main_command",))
 
-        bindings = {
-            "_cli_dispatch": types.SimpleNamespace(
-                dispatch_desktop_command=dispatch_desktop_command,
-                dispatch_main_command=dispatch_main_command,
-            ),
-        }
-        for name in [
-            "cmd_desktop_install",
-            "cmd_desktop_doctor",
-            "cmd_desktop_status",
-            "cmd_desktop_config",
-            "cmd_desktop_recent",
-            "cmd_desktop_proof",
-            "cmd_desktop_publish",
-            "cmd_desktop_cleanup",
-            "cmd_desktop_smoke",
-            "cmd_desktop_click",
-            "cmd_desktop_inspect",
-            "cmd_enqueue",
-            "cmd_drain",
-            "cmd_run",
-            "cmd_ship",
-            "cmd_check",
-            "cmd_list",
-            "cmd_bump",
-            "cmd_cancel",
-            "cmd_logs",
-            "cmd_cleanup",
-            "cmd_evidence",
-            "cmd_status",
-            "cmd_cloud_workflows",
-            "cmd_cloud_defaults",
-            "cmd_cloud_history",
-            "cmd_cloud_compare",
-            "cmd_cloud_recommend",
-            "cmd_cloud_run",
-            "cmd_cloud_status",
-            "cmd_cloud_namespace_doctor",
-            "cmd_cloud_namespace_setup",
-        ]:
-            bindings[name] = object()
+    def test_install_cli_dispatch_helpers_preserves_unknown_fallbacks(self):
+        bindings = {"custom": object()}
 
-        self.mod.install_cli_dispatch_helpers(bindings, ("cmd_desktop", "dispatch_main_command"))
+        with mock.patch.object(self.mod, "install_local_helpers") as install_local:
+            self.mod.install_cli_dispatch_helpers(bindings, ("custom",))
 
-        self.assertEqual(bindings["cmd_desktop"](args), 44)
-        self.assertEqual(bindings["dispatch_main_command"](args, lambda: None), 55)
-        self.assertEqual(bindings["cmd_desktop"].__name__, "cmd_desktop")
-        self.assertEqual(bindings["dispatch_main_command"].__name__, "dispatch_main_command")
+        install_local.assert_called_once_with(bindings, self.mod.__dict__, ("custom",))
 
 
 if __name__ == "__main__":
