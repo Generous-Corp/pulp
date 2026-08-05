ea299b54ca5e32a444a2a9caaaead1e00f556cae
daniel raffel <daniel.raffel@gmail.com>
2026-08-05 16:38:17 -0700

    feat: add deterministic music voicing primitives

--- core/music/include/pulp/music/music.hpp
+++ core/music/include/pulp/music/music.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <pulp/music/chord.hpp>
+#include <pulp/music/harmony.hpp>
 #include <pulp/music/markov.hpp>
 #include <pulp/music/pattern.hpp>
 #include <pulp/music/pitch.hpp>
 #include <pulp/music/rhythm_relationship.hpp>
+#include <pulp/music/spelling.hpp>
+#include <pulp/music/voicing.hpp>
